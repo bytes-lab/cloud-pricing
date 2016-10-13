19cd6d1cec518acd399ea23fbb3bd993640f1491
Thomas Andrew <jason.5001001@gmail.com>
2016-10-14 07:48:49 +1100

    CORS and POST added

--- cloud_pricing/views.py
+++ cloud_pricing/views.py
@@ -1,12 +1,15 @@
 import json
 
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from digital_ocean_pricing import get_price_DO_droplet
 from ec2_pricing import get_price_on_demand_ec2_instance
 
 
+@csrf_exempt
 def get_cloud_pricing(request):
-    if request.method == 'GET':
+    if request.method in ['GET', 'POST']:
         result = []
         params = json.loads(request.body)
         for param in params:
@@ -20,6 +23,12 @@ def get_cloud_pricing(request):
                 result_['error'] = 'Please specify correct cloud type. e.g) `cloud`: `aws ec2` or `digital ocean`'
             result.append(result_)
 
-        return HttpResponse(json.dumps(result, 'application/json'))
+        response = HttpResponse(json.dumps(result, 'application/json'))
+
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Headers"] = "accept, access-control-allow-headers, access-control-allow-methods, access-control-allow-origin, authid, authorization, content-type"
+        response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+
+        return response
     else:
         return HttpResponse("This api supports only GET.", 'application/json')
