2b4c738e5652acd950b9715c8831e62bbcdd4982
Thomas Andrew <jason.5001001@gmail.com>
2016-10-14 22:38:12 +1100

    revise method check

--- cloud_pricing/views.py
+++ cloud_pricing/views.py
@@ -9,26 +9,26 @@ from ec2_pricing import get_price_on_demand_ec2_instance
 
 @csrf_exempt
 def get_cloud_pricing(request):
-    if request.method in ['GET', 'POST']:
-        result = []
-        params = json.loads(request.body)
-        for param in params:
-            cloud = param.get('cloud')
-            if cloud == 'digital ocean':
-                result_ = get_price_DO_droplet(param)
-            elif cloud == 'aws ec2':
-                result_ = get_price_on_demand_ec2_instance(param)
-            else:
-                result_ = dict(param)
-                result_['error'] = 'Please specify correct cloud type. e.g) `cloud`: `aws ec2` or `digital ocean`'
-            result.append(result_)
+    # if request.method in ['GET', 'POST']:
+    result = []
+    params = json.loads(request.body)
+    for param in params:
+        cloud = param.get('cloud')
+        if cloud == 'digital ocean':
+            result_ = get_price_DO_droplet(param)
+        elif cloud == 'aws ec2':
+            result_ = get_price_on_demand_ec2_instance(param)
+        else:
+            result_ = dict(param)
+            result_['error'] = 'Please specify correct cloud type. e.g) `cloud`: `aws ec2` or `digital ocean`'
+        result.append(result_)
 
-        response = HttpResponse(json.dumps(result, 'application/json'))
+    response = HttpResponse(json.dumps(result, 'application/json'))
 
-        response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Headers"] = "accept, access-control-allow-headers, access-control-allow-methods, access-control-allow-origin, authid, authorization, content-type"
-        response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
+    response["Access-Control-Allow-Origin"] = "*"
+    response["Access-Control-Allow-Headers"] = "accept, access-control-allow-headers, access-control-allow-methods, access-control-allow-origin, authid, authorization, content-type"
+    response["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
 
-        return response
-    else:
-        return HttpResponse("This api supports only GET.", 'application/json')
+    return response
+    # else:
+    #     return HttpResponse("This api supports only GET.", 'application/json')
